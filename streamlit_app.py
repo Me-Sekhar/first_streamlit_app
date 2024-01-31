6f8f4f892ac7d21266eb18efa56667817baf8fff
Me-Sekhar <82931724+Me-Sekhar@users.noreply.github.com>
2024-01-31 16:38:55 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Oatmeal')
-streamlit.text('Bread Omellete')
-streamlit.text('Croissant')
+streamlit.text('🥗 Kale spinach smoothie')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
