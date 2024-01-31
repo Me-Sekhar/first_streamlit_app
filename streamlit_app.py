fbe5c9ccf673ba1476ab4477ba4c69c06d68ee91
Me-Sekhar <82931724+Me-Sekhar@users.noreply.github.com>
2024-01-31 16:55:16 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 # The picker works, but the numbers don't make any sense! We want the customer to be able to choose the fruits by name!!
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
