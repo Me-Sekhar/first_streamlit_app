d688c00d1b0cbe7d07000a3221d5b74969249a5d
Me-Sekhar <82931724+Me-Sekhar@users.noreply.github.com>
2024-01-31 16:36:28 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal')
+streamlit.text('🥣 Oatmeal')
 streamlit.text('Bread Omellete')
 streamlit.text('Croissant')
